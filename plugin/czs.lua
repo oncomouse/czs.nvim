135e948e5ed7a8f889dc072cd46da8c4c764b57f
oncomouse <oncomouse@gmail.com>
2023-01-02 12:00:19 -0600

    Gif

--- plugin/czs.lua
+++
@@ -1,7 +0,0 @@
-if vim.opt.cmdheight:get() == 0 then
-	require("czs").setup()
-	if not vim.g.czs_do_not_map then
-		vim.keymap.set("n", "n", "<Plug>(czs-move-n)")
-		vim.keymap.set("n", "N", "<Plug>(czs-move-N)")
-	end
-end
